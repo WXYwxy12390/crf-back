90e0e7e0296b8023f0967d90428b2a4d0b14ada4
yours <1851861899@qq.com>
2020-10-03 09:26:50 +0800

    身份证重复的bug

--- app/api/v1/base_line/patient.py
+++ app/api/v1/base_line/patient.py
@@ -1,6 +1,7 @@
 from flask import request
 
 from app.libs.error import Success
+from app.libs.error_code import ParameterException
 from app.libs.redprint import Redprint
 from app.models import json2db
 from app.models.base_line import Patient
@@ -16,5 +17,9 @@ def get_patient(id):
 @api.route('/<int:id>',methods = ['POST'])
 def add_patient(id):
     data = request.get_json()
+    if 'idNumber' in data:
+        patient = Patient.query.filter(Patient.is_delete==0,Patient.idNumber==data['idNumber'],Patient.id!=id).first()
+        if patient:
+            raise ParameterException(msg='已经存在相同身份证号码。')
     json2db(data, Patient)
     return Success()
