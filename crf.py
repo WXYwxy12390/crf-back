bc01bd90450a035359ed9aecf21c1c57a54c33a0
kid <kid@hust.edu.cn>
2021-06-15 15:34:40 +0800

    fix: 添加https证书

--- crf.py
+++ crf.py
@@ -44,4 +44,5 @@ def framework_error(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=40585)
+    app.run(debug=True, host='0.0.0.0', port=40585,ssl_context=('server.pem','server.key'))
+    
