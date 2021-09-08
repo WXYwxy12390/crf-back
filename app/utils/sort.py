180290e204f5940a49361a4a13bb32aa39bbf860
WXYwxy12390 <573375255@qq.com>
2021-09-08 23:58:56 +0800

    用于v1/sample/all 增加排序方式

---
+++ app/utils/sort.py
@@ -0,0 +1,18 @@
+from app.models.base_line import Patient
+
+
+def sort_samples_while_query(after_filter, way):
+    if not way:
+        return after_filter.order_by(Patient.update_time.desc()).all()
+    elif way == 1:
+        # 按患者编号倒序
+        return after_filter.order_by(Patient.patNumber.desc()).all()
+    elif way == 2:
+        # 按患者编号正序
+        return after_filter.order_by(Patient.patNumber.asc()).all()
+    elif way == 3:
+        # 按创建时间倒序排列
+        return after_filter.order_by(Patient.create_time.desc()).all()
+    elif way == 4:
+        # 按创建时间正序排列
+        return after_filter.order_by(Patient.create_time.asc()).all()
