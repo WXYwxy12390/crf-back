fc2b933d0f2960c243e07bfde7dda6518544489a
WXYwxy12390 <573375255@qq.com>
2021-10-16 14:08:36 +0800

    获取样本时添加两种排序方式：按更新时间正序和倒叙

--- app/utils/sort.py
+++ app/utils/sort.py
@@ -14,5 +14,9 @@ def sort_samples_while_query(after_filter, way):
     elif way == 4:
         # 按创建时间正序排列
         return after_filter.order_by(Patient.create_time.asc()).all()
+    elif way == 5:
+        # 按更新时间正序排列
+        return after_filter.order_by(Patient.update_time.asc()).all()
     else:
+        # 按更新时间倒叙排列
         return after_filter.order_by(Patient.update_time.desc()).all()
